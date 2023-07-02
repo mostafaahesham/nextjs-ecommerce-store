fefbf0c3461409f3f5b0a41399aaedc4fd20b35b
Mostafa <mostafahesham.wm@gmail.com>
2023-07-02 16:26:41 +0200

    fetching products

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,41 @@
 "use client";
 import Product from "@/components/Product";
+import Axios from "axios";
+import { styled } from "styled-components";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  return <Product />;
+  const [products, setProducts] = useState("");
+
+  useEffect(() => {
+    Axios.get("http://localhost:8000/api/v1/products?limit=100").then((res) => {
+      console.log(res.data.data);
+      setProducts(res.data.data);
+    });
+  }, []);
+  return (
+    <DisplayProductsContainer>
+      {products
+        ? products.map((product) => (
+            <Product
+              key={product.id}
+              name={product.name}
+              description={product.description}
+              oldPrice={product.currentPrice}
+              newPrice={product.discountedPrice}
+              image={product.variants[0].variantImage}
+              brandImage={product.brand.image}
+              brandName={product.brand.name}
+            />
+          ))
+        : null}
+    </DisplayProductsContainer>
+  );
 }
+
+const DisplayProductsContainer = styled.div`
+  display: grid;
+  gap: 2em;
+  width: 50vw;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+`;
